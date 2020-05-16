83c414818048b89dc2611d466dec61e023124dc9
priyajaregi <65464423+priyajaregi@users.noreply.github.com>
2020-05-17 00:48:31 +0530

    Update programme1.cpp

--- programme1.cpp
+++ programme1.cpp
@@ -1,1 +1,32 @@
 
+void find ()
+{
+int i,k,n,f,a [50];
+for(i=0;i <n;++i)
+{
+cin>>a [i];
+}
+cout <<"Element to be check="<<k;
+cin>>k;
+f=0;
+for (i=0;i<n;++i)
+{
+if (k==a[i])
+{
+f=1;
+break;
+}
+if (f==1)
+{
+cout <<"element found";
+else 
+{
+cout<<"element not present ";
+}}}}
+int main ()
+{
+int n;
+cin>>n;
+find (n)
+return 0;
+}
