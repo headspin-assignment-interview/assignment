c5a4d02aa7269bcdcdb29c7148718c7f082e2352
priyajaregi <65464423+priyajaregi@users.noreply.github.com>
2020-05-17 00:55:36 +0530

    Update programme2.cpp

--- programme2.cpp
+++ programme2.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+#include<studio.h>
+int main ()
+{
+int i,k,n;
+cout <<"Enternumber of digits in the input number ";
+cin>>k;
+cout <<"Enter the number";
+cin>>n;
+for (i=0;i <k;i++)
+{
+a [i]=n%10;
+n=n/10;
+}
+for (i=k-1;i <k;i--)
+{
+cout <<a [i]
+}
+return 0;
+}
