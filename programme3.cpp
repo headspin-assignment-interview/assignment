65d1cf36f49a2aed01179010ec4b7c7626096f78
priyajaregi <65464423+priyajaregi@users.noreply.github.com>
2020-05-17 01:00:54 +0530

    Update programme3.cpp

--- programme3.cpp
+++ programme3.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+#include<stdio.h>
+int main()
+{
+int i;
+int n;
+int sum=1;
+cout<<“enter n value”;
+cin>>n;
+for (i=1;i<n;++i)
+{
+sum=sum+(1/((2*i)+1));
+}
+cout <<sum;
+return 0;
+}
